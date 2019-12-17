501e19da22f1448f9d7ca7ca592d189de8e0011a
nathyyo <46191581+nathyyo@users.noreply.github.com>
2019-12-17 11:56:43 +0300

    adding boot strap

--- functions.php
+++ functions.php
@@ -125,6 +125,9 @@ function where_beauty_meets_design_scripts() {
 	wp_enqueue_script( 'where-beauty-meets-design-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'where-beauty-meets-design-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    
+  wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array( 'jquery' ) );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
